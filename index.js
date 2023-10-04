2b8b24af7ccf8916625b35086ea2c209f2213e31
rifat-sarker <rifatswd@gmail.com>
2023-10-04 21:27:02 +0600

    modal added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let total = 0;
-function handleClickOnCards(target){
+
+function handleClickOnCards(target) {
     const selectedProductsContainer = document.getElementById('selected-products');
     const productName = target.childNodes[1].innerText;
     const li = document.createElement('li');
@@ -7,51 +8,50 @@ function handleClickOnCards(target){
     selectedProductsContainer.appendChild(li);
 
     const price = target.childNodes[3].innerText.split(' ')[0];
-    
+
     total = parseInt(total) + parseInt(price);
-    
+
 
     const firstTotal = document.getElementById('total-price');
     firstTotal.innerText = total;
-    
+
     // if total > 0 then purchase button will become enabled
     const makePurchase = document.getElementById('make-purchase');
-    if(total > 0){
+    if (total > 0) {
         makePurchase.removeAttribute('disabled');
-    }
-    else{
+    } else {
         makePurchase.setAttribute('disabled', true);
     }
 
     // if total > 200 apply button will become enabled
     const applyButton = document.getElementById('apply-button');
-    if(total >= 200){
+    if (total >= 200) {
         applyButton.removeAttribute('disabled')
-    }
-    else{
+    } else {
         applyButton.setAttribute('disabled', true);
     }
 
-    document.getElementById('apply-button').addEventListener('click', function(){
-        const discountPrice = document.getElementById('discount');
-        const discountPercentage = 20 / 100 ;
-        const discountTotal = total * discountPercentage;
-        const discountTotalPrice = total - discountTotal;
-        discountPrice.innerText = discountTotalPrice;
-    
+    document.getElementById('apply-button').addEventListener('click', function () {
+        const inputField = document.getElementById('input-field');
+        const text = inputField.value;
+        if (text === 'SELL200') {
+            const discountPrice = document.getElementById('discount');
+            const discountPercentage = 20 / 100;
+            const discountTotal = total * discountPercentage;
+
+            const discount = discountPrice.innerText = discountTotal;
+            const totalFinalPrice = document.getElementById('total');
+            const totalFinal = total - discountTotal;
+            const finalTotal = totalFinalPrice.innerText = totalFinal;
+        } else {
+            alert('Wrong coupon code.Please try again!')
+        }
+
+    })
+
+    document.getElementById('make-purchase').addEventListener('click', function () {
+
     })
 }
-// document.getElementById('input-field').addEventListener('keyup', function(event){
-//     const text = event.target.value;
-//     const applyButton = document.getElementById('apply-button');
-//     if( text === 'SELL200'){
-//         applyButton.removeAttribute('disabled');
-//     }
-//     else{
-//         applyButton.setAttribute('disabled', true);
-//     }
-    
-// })
-
 
 
