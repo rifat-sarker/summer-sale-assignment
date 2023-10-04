51dfcf4cc5d4d86c22e0a24a7e003a38a3e43f29
rifat-sarker <rifatswd@gmail.com>
2023-10-04 18:13:28 +0600

    total price added

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
+let total = 0;
 function handleClickOnCards(target){
     const selectedProductsContainer = document.getElementById('selected-products');
     const productName = target.childNodes[1].innerText;
     const li = document.createElement('li');
     li.innerText = productName;
     selectedProductsContainer.appendChild(li);
+
+    const price = target.childNodes[3].innerText.split(' ')[0];
+    
+    total = parseInt(total) + parseInt(price);
+    console.log(total);
+
+    const firstTotal = document.getElementById('total-price');
+    firstTotal.innerText = total;
 }
 
